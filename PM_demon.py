87be32ab6858fdd3b0d69eb5be63ea2fd7c7edbe
SeiyaIwabuchi <0123ook@gmail.com>
2020-07-27 21:45:15 +0900

    テスト前コミット

--- PM_demon.py
+++ PM_demon.py
@@ -1,13 +1,51 @@
-import clr
-clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import Application
+import time
+import subprocess
+import signal
 import threading
-class PM_Demon:
-    """
-    ifttt連携してPCの電源を管理するのが目的
-    10秒おきにサーバーにリクエストの状態を問い合わせ、それに応じてスリープに入れる
-    """
-    def __init__(self):
-        delay = 10 #十秒後にスリープに入るってこと
-    @classmethod
-    def 
+import requests
+import sys
+
+class PM_Daemon:
+	"""
+	ifttt連携してPCの電源を管理するのが目的
+	10秒おきにサーバーにリクエストの状態を問い合わせ、それに応じてスリープに入れる
+	"""
+	def __init__(self):
+		self.delay = 10 #十秒後にスリープに入るってこと
+	def sleeping(self):
+		#スタンバイ状態にする
+		time.sleep(self.delay)
+		requests.get("http://127.0.0.1:8080/setState/1")
+		Application.SetSuspendState(PowerState.Suspend, False, False)
+	def daemon(self):
+		while True:
+			try:
+				r = requests.get("http://127.0.0.1:8080/getState")
+				print(r.text)
+				if int(r.text) == 0:
+					self.sleeping()
+				elif int(r.text) == 2:
+					requests.get("http://127.0.0.1:8080/setState/3")
+			except requests.ConnectionError:
+				print("requests.ConnectionError")
+			finally:
+				time.sleep(10)
+
+
+if __name__ == "__main__":
+	if sys.argv[1] == "server" or sys.argv[1] == "debug":
+		server = subprocess.Popen(["python","module1.py"],shell=True)
+	if sys.argv[1] == "client" or sys.argv[1] == "debug":
+		import clr
+		clr.AddReference("System.Windows.Forms")
+		from System.Windows.Forms import Application,PowerState
+		d = PM_Daemon()
+	try:
+		if  sys.argv[1] == "server":
+			while True:
+				pass
+		else:
+			d.daemon()
+	finally:
+		if  sys.argv[1] == "server" or sys.argv[1] == "debug":
+			server.send_signal(signal.CTRL_C_EVENT)
---
+++ module1.py
@@ -0,0 +1,54 @@
+from flask import Flask
+import datetime
+from config import config
+import socket
+import struct
+from traceback import print_exc
+import threading
+import time
+
+app = Flask(__name__)
+state = 1
+@app.route('/')
+def index():
+	return str(datetime.datetime.now())
+
+@app.route("/getState")
+def getState():
+	return str(state)
+
+@app.route("/setState/<int:value>")
+def setState(value=None):
+	global state
+	state = value
+	return str(state)
+
+DEFAULT_PORT = 7
+def send_magic_packet(addr):
+   # create socket
+   with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+       s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+       # parse address
+       mac_ = addr.upper().replace("-", "").replace(":", "")
+       if len(mac_) != 12:
+           raise Exception("invalid MAC address format: {}".format(addr))
+       buf_ = b'f' * 12 + (mac_ * 20).encode()
+       # encode to magic packet payload
+       magicp = b''
+       for i in range(0, len(buf_), 2):
+           magicp += struct.pack('B', int(buf_[i:i + 2], 16))
+
+       # send magic packet
+       print("sending magic packet for: {}".format(addr))
+       s.sendto(magicp, ('<broadcast>', DEFAULT_PORT))
+
+def deamon():
+	while True:
+		if state == 2:
+			try:
+				send_magic_packet(config.mac)
+			except:
+				print_exc()
+
+if __name__ == "__main__":
+    app.run(debug=config.debug,host=config.host,port=config.port)
